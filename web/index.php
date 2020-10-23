216670e2953a1fc60b1291c9d352860f54e62ac1
Sergei Mono <sergei.ogarkov@sogarkov.ru>
2020-10-23 15:46:28 +0300

    App run params customization

--- web/index.php
+++ web/index.php
@@ -1,9 +1,9 @@
 <?php
 
 // comment out the following two lines when deployed to production
-$credentials = require __DIR__ . '/../config/credentials.php';
-defined('YII_DEBUG') or define('YII_DEBUG', $credentials['YII_DEBUG']);
-defined('YII_ENV') or define('YII_ENV', $credentials['YII_ENV']);
+$custom_params = require __DIR__ . '/../config/custom_params.php';
+defined('YII_DEBUG') or define('YII_DEBUG', $custom_params['YII_DEBUG']);
+defined('YII_ENV') or define('YII_ENV', $custom_params['YII_ENV']);
 
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
